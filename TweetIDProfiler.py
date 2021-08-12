bf95a0c179d89e4f62a518ba92d371b96f1e1fb5
PJH <54723781+pjh-github@users.noreply.github.com>
2021-08-12 16:28:10 +0100

    Update TweetIDProfiler.py

--- TweetIDProfiler.py
+++ TweetIDProfiler.py
@@ -1,25 +1,5 @@
 ## Goal - Extract timestamp, datacenter, server and sequence from tweet ID.
 
-## Assumptions -
-## This is accurate: https://twitter.com/conspirator0/status/1227063249440509952/photo/1
-## Tweets can be split up into the given structure.
-
-## Methodology -
-
-  ## Extract tweet ID
-
-  ## Take URL: https://twitter.com/realDonaldTrump/status/1226595698793230348
-  ## Actual tweet date - 7:56 PM Feb 9, 2020
-  ## Split by '/', take the final component
-
-  ## Convert to Binary
-
-  ## Substring binary into timestamp, datacenter, server and sequence. Pass to given timestamp logic.
-
-## Twitter IDs - More info here: https://developer.twitter.com/en/docs/basics/twitter-ids 
-
-## Todo - Hook up with this for some statistical analysis: https://github.com/hhromic/python-twitter-toolbox
-
 import datetime 
 
 ## Logic given from twitter thread, takes tweet ID as an input.  
